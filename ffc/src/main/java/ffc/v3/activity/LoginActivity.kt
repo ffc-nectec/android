4fd089f6e16a7e757361626a5de30014cc7f6cfb
Nat Su <natthanicha.suriyamongkol@nectec.or.th>
2018-07-12 08:16:01 +0700

    Create "Activity Package"

---
+++ ffc/src/main/java/ffc/v3/activity/LoginActivity.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ffc.v3.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import ffc.v3.MapsActivity
+import ffc.v3.R
+import ffc.v3.authen.LoginPresenter
+import ffc.v3.fragment.LoginOrgFragment
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.intentFor
+
+class LoginActivity : AppCompatActivity(), LoginPresenter {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setBlurImage()
+        initInstances()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.contentContainer, LoginOrgFragment(), "LoginOrg")
+                .commit()
+        }
+    }
+
+    private fun initInstances() {
+
+    }
+
+    private fun setBlurImage() {
+        Glide.with(this)
+            .load(R.drawable.community)
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(4, 3)))
+            .into(ivCommunity)
+    }
+
+    override fun onLoginSuccess() {
+        startActivity(intentFor<MapsActivity>())
+    }
+
+    override fun onError(message: String) {
+    }
+
+}
