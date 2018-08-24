00ea7200e549774e21501a1ac1855d52a470a9b3
Piruin Panichphol <piruin.p@gmail.com>
2018-08-24 14:08:23 +0700

    add user on IdentityRepo

--- ffc/src/main/java/ffc/v3/authen/IdentityRepo.kt
+++ ffc/src/main/java/ffc/v3/authen/IdentityRepo.kt
@@ -2,6 +2,7 @@ package ffc.v3.authen
 
 import android.content.Context
 import ffc.entity.Organization
+import ffc.entity.User
 import ffc.v3.util.get
 import ffc.v3.util.put
 import org.jetbrains.anko.defaultSharedPreferences
@@ -11,19 +12,30 @@ interface IdentityRepo {
     var org: Organization?
 
     var token: String?
+
+    var user: User?
 }
 
 private class PreferenceIdentityRepo(context: Context) : IdentityRepo {
+
     var preference = context.defaultSharedPreferences
+
     override var org: Organization?
         get() = preference.get<Organization>("org")
         set(value) {
             preference.edit().put("org", value).apply()
         }
+
     override var token: String?
         get() = preference.getString("token", null)
         set(value) {
             preference.edit().putString("token", value).apply()
+        }
+
+    override var user: User?
+        get() = preference.get<User>("user")
+        set(value) {
+            preference.edit().put("user", value).apply()
         }
 }
 
