5d2a3a7efce9ff351bdbfde8feb24dae1723b942
Piruin Panichphol <piruin.p@gmail.com>
2018-09-14 15:11:25 +0700

    can show search result as person item

---
+++ ffc/src/main/kotlin/ffc/app/person/PersonAdapter.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ffc.app.person
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import ffc.android.layoutInflater
+import ffc.app.R
+import ffc.entity.Person
+import ffc.util.AdapterClickListner
+import kotlinx.android.synthetic.main.person_list_item.view.personNameView
+
+class PersonAdapter(val persons: List<Person>, val onClickDsl: AdapterClickListner<Person>.() -> Unit) : RecyclerView.Adapter<PersonAdapter.PersonHolder>() {
+
+    val listener = AdapterClickListner<Person>().apply(onClickDsl)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonHolder {
+        val view = parent.layoutInflater.inflate(R.layout.person_list_item, parent, false)
+        return PersonHolder(view)
+    }
+
+    override fun getItemCount() = persons.size
+
+    override fun onBindViewHolder(holder: PersonHolder, position: Int) {
+        holder.bind(persons[0], listener)
+    }
+
+    class PersonHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(person: Person, listener: AdapterClickListner<Person>) {
+            with(person) {
+                itemView.personNameView.text = name
+                listener.bindOnItemClick(itemView, person)
+            }
+        }
+    }
+}
+
+
+
