408ec54c78519e24aea4e483beeee361a136d6c7
Piruin Panichphol <piruin.p@gmail.com>
2018-10-08 11:32:56 +0700

    show house's resident list

---
+++ ffc/src/main/kotlin/ffc/app/location/HouseApi.kt
@@ -0,0 +1,15 @@
+package ffc.app.location
+
+import ffc.entity.Person
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface HouseApi {
+
+    @GET("org/{orgId}/house/{houseId}/resident")
+    fun personInHouse(
+        @Path("orgId") orgId: String,
+        @Path("houseId") houseId: String
+    ): Call<List<Person>>
+}
