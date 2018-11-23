b022997d05a123c2054c31c8c3bc365d709f914e
Piruin Panichphol <piruin.p@gmail.com>
2018-11-23 14:21:10 +0700

    show relationship as grid 2 col

--- ffc/src/main/kotlin/ffc/app/person/RelationshipFragment.kt
+++ ffc/src/main/kotlin/ffc/app/person/RelationshipFragment.kt
@@ -2,8 +2,7 @@ package ffc.app.person
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -29,9 +28,8 @@ class RelationshipFragment : Fragment() {
 
         person?.let {
             with(recycleView) {
-                adapter = RelationshipAdapter(it.relationships)
-                layoutManager = LinearLayoutManager(context!!)
-                addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                adapter = RelationshipAdapter(it.relationships.sortedBy { it.relate })
+                layoutManager = GridLayoutManager(context!!, 2)
             }
 
             genogramButton.onClick {
