be62dc4fdbcfb5fddd5c62b1e3b35a972f3d9f6a
Piruin Panichphol <piruin.p@gmail.com>
2018-11-29 16:54:07 +0700

    show more information of healthCareservice

--- ffc/src/main/kotlin/ffc/app/healthservice/HealthValueAdapter.kt
+++ ffc/src/main/kotlin/ffc/app/healthservice/HealthValueAdapter.kt
@@ -2,24 +2,42 @@ package ffc.app.healthservice
 
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
+import android.support.annotation.LayoutRes
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import ffc.android.inflate
+import ffc.app.R
 
-class HealthValueAdapter(val values: List<Value>) : RecyclerView.Adapter<ValueItemViewHolder>() {
+class HealthValueAdapter(
+    val values: List<Value>,
+    val style: Style = Style.GRID,
+    val showOnlyFistLabel: Boolean = false
+) : RecyclerView.Adapter<ValueItemViewHolder>() {
+
+    enum class Style(@LayoutRes val layoutRes: Int) {
+        GRID(R.layout.value_grid_item),
+        SMALL(R.layout.value_small_item)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, type: Int): ValueItemViewHolder {
-        return ValueItemViewHolder(parent)
+        return ValueItemViewHolder(parent.inflate(style.layoutRes, false))
     }
 
     override fun getItemCount() = values.size
 
+    var lastLabel: String? = null
+
     override fun onBindViewHolder(holder: ValueItemViewHolder, position: Int) {
-        holder.bind(values[position])
+        val item = values[position]
+        holder.bind(item)
+        if (showOnlyFistLabel && item.label == lastLabel)
+            holder.hindLabel()
+        lastLabel = item.label
     }
 }
 
 class Value(
-    val label: String,
+    val label: String? = null,
     val value: String = "-",
     val caption: String? = null,
     @ColorInt val color: Int? = null,
