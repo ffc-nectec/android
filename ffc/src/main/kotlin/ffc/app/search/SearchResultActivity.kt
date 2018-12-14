6c3347d6b62eaae4938254bc6ea28f50c4f7186d
Piruin Panichphol <piruin.p@gmail.com>
2018-12-14 19:59:43 +0700

    can show result in same page

--- ffc/src/main/kotlin/ffc/app/search/SearchResultActivity.kt
+++ ffc/src/main/kotlin/ffc/app/search/SearchResultActivity.kt
@@ -18,41 +18,13 @@
 package ffc.app.search
 
 import android.app.SearchManager
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
 import android.content.Intent
 import android.os.Bundle
 import android.provider.SearchRecentSuggestions
-import android.support.design.widget.TabLayout
-import android.support.v7.widget.LinearLayoutManager
-import android.widget.ImageView
-import ffc.android.addVeriticalItemDivider
-import ffc.android.gone
-import ffc.android.observe
-import ffc.android.onClick
-import ffc.android.sceneTransition
-import ffc.android.viewModel
-import ffc.android.visible
+import ffc.android.find
 import ffc.app.FamilyFolderActivity
 import ffc.app.R
 import ffc.app.dev
-import ffc.app.location.HouseActivity
-import ffc.app.location.HouseAdapter
-import ffc.app.person.PersonAdapter
-import ffc.app.person.startPersonActivityOf
-import ffc.entity.Person
-import ffc.entity.place.House
-import kotlinx.android.synthetic.main.activity_search_result.contentSwitcher
-import kotlinx.android.synthetic.main.activity_search_result.emptyView
-import kotlinx.android.synthetic.main.activity_search_result.emptyView2
-import kotlinx.android.synthetic.main.activity_search_result.moreHouseButton
-import kotlinx.android.synthetic.main.activity_search_result.searchResultView
-import kotlinx.android.synthetic.main.activity_search_result.searchResultView2
-import kotlinx.android.synthetic.main.activity_search_result.tabLayout
-import org.jetbrains.anko.dimen
-import org.jetbrains.anko.find
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.toast
 
 class SearchResultActivity : FamilyFolderActivity() {
 
@@ -61,8 +33,6 @@ class SearchResultActivity : FamilyFolderActivity() {
         set(value) {
             putExtra(SearchManager.QUERY, value)
         }
-
-    private val personViewModel by lazy { viewModel<SimpleViewModel>() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,7 +43,6 @@ class SearchResultActivity : FamilyFolderActivity() {
                 intent.query = "พิรุณ"
             }
         }
-        observeViewModelData()
 
         with(supportActionBar!!) {
             title = intent.query
@@ -81,72 +50,6 @@ class SearchResultActivity : FamilyFolderActivity() {
             onToolbarClick { onBackPressed() }
         }
         handleIntent(intent)
-        tabLayout.addOnTabSelectedListener(onTabSelect)
-    }
-
-    private val onTabSelect = object : TabLayout.BaseOnTabSelectedListener<TabLayout.Tab> {
-        override fun onTabReselected(tab: TabLayout.Tab) {}
-
-        override fun onTabUnselected(tab: TabLayout.Tab) {}
-
-        override fun onTabSelected(tab: TabLayout.Tab) {
-            when (tab.position) {
-                0 -> contentSwitcher.showPrevious()
-                1 -> contentSwitcher.showNext()
-            }
-        }
-    }
-
-    private fun observeViewModelData() {
-        observe(personViewModel.persons) {
-            if (it.isNullOrEmpty()) {
-                bindAdapter(listOf())
-                emptyView.showEmpty()
-            } else {
-                bindAdapter(it)
-                emptyView.showContent()
-            }
-        }
-        observe(personViewModel.pLoading) { if (it == true) emptyView.showLoading() }
-        observe(personViewModel.pException) {
-            it?.let {
-                emptyView.error(it).show()
-            }
-        }
-
-        observe(personViewModel.houses) {
-            if (it.isNullOrEmpty()) {
-                emptyView2.showEmpty()
-                bindHouseAdapter(listOf())
-            } else {
-                emptyView2.showContent()
-                bindHouseAdapter(it)
-
-            }
-        }
-        observe(personViewModel.hLoading) { if (it == true) emptyView2.showLoading() }
-        observe(personViewModel.hException) {
-            it?.let { throwable ->
-                emptyView2.error(throwable).show()
-            }
-        }
-    }
-
-    private fun bindHouseAdapter(it: List<House>) {
-        with(searchResultView2) {
-            layoutManager = LinearLayoutManager(context)
-            addVeriticalItemDivider(dimen(R.dimen.content_start_horizontal_padding))
-            adapter = HouseAdapter(it, limit = 5) {
-                val intent = intentFor<HouseActivity>("houseId" to it.id)
-                startActivity(intent, sceneTransition())
-            }
-            if (it.size > 5) {
-                moreHouseButton.visible()
-                moreHouseButton.onClick { toast("show more house") }
-            } else {
-                moreHouseButton.gone()
-            }
-        }
     }
 
     override fun onNewIntent(intent: Intent) {
@@ -159,35 +62,12 @@ class SearchResultActivity : FamilyFolderActivity() {
             SearchRecentSuggestions(this, RecentSearchProvider.AUTHORITY, RecentSearchProvider.MODE)
                 .saveRecentQuery(query, null)
             supportActionBar!!.title = query
-            personViewModel.pLoading.value = true
-            personSearcher(org!!.id).search(query!!) {
-                always { personViewModel.pLoading.value = false }
-                onFound { personViewModel.persons.value = it }
-                onNotFound { personViewModel.persons.value = listOf() }
-                onFail { personViewModel.pException.value = it }
-            }
 
-            personViewModel.hLoading.value = true
-            houseSearcher(org!!.id).search(query) {
-                always { personViewModel.hLoading.value = false }
-                onFound { personViewModel.houses.value = it }
-                onNotFound { personViewModel.houses.value = listOf() }
-                onFail { personViewModel.hException.value = it }
-            }
+            val personResult = supportFragmentManager.find<PersonSearchResultFragment>(R.id.personResult)
+            personResult.query = query
+            val houseResult = supportFragmentManager.find<HouseSearchResultFragment>(R.id.houseResult)
+            houseResult.query = query
         } else {
-            personViewModel.pException.value = IllegalArgumentException("ไม่มีคำค้นหา")
-            personViewModel.hException.value = IllegalArgumentException("ไม่มีคำค้นหา")
-        }
-    }
-
-    private fun bindAdapter(persons: List<Person>) {
-        searchResultView.layoutManager = LinearLayoutManager(this)
-        searchResultView.addVeriticalItemDivider(dimen(R.dimen.content_start_horizontal_padding))
-        searchResultView.adapter = PersonAdapter(persons, limit = 5) {
-            onItemClick { p ->
-                startPersonActivityOf(p, null,
-                    find<ImageView>(R.id.personImageView) to getString(R.string.transition_person_profile))
-            }
         }
     }
 
@@ -200,15 +80,5 @@ class SearchResultActivity : FamilyFolderActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(android.R.anim.fade_in, 0)
-    }
-
-    class SimpleViewModel : ViewModel() {
-        val persons = MutableLiveData<List<Person>>()
-        val pLoading = MutableLiveData<Boolean>()
-        val pException = MutableLiveData<Throwable>()
-
-        val houses = MutableLiveData<List<House>>()
-        val hLoading = MutableLiveData<Boolean>()
-        val hException = MutableLiveData<Throwable>()
     }
 }
