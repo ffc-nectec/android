3cbafc435cda43c22b6ef6dbe374c038a9be171e
Piruin Panichphol <piruin.p@gmail.com>
2018-12-14 21:44:41 +0700

    Can show more search result

---
+++ ffc/src/main/kotlin/ffc/app/search/MoreResultFragment.kt
@@ -0,0 +1,63 @@
+package ffc.app.search
+
+import android.os.Bundle
+import ffc.android.getAs
+import ffc.android.requestScroll
+import ffc.entity.Entity
+import ffc.entity.Person
+import ffc.entity.gson.toJson
+import ffc.entity.place.House
+import kotlinx.android.synthetic.main.search_result_fragment.header
+import org.jetbrains.anko.bundleOf
+
+interface MoreResultFragment<T : Entity> {
+
+    fun setArgument(query: String, result: List<T>)
+}
+
+class PersonMoreResultFragment : PersonSearchResultFragment(), MoreResultFragment<Person> {
+
+    override val limit: Int = Int.MAX_VALUE
+
+    override val title: String
+        get() = """คน "${arguments!!.getString("query")}" """.trim()
+
+    val result: List<Person>
+        get() = arguments!!.getAs("result")!!
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.content.value = result
+    }
+
+    override fun setArgument(query: String, result: List<Person>) {
+        arguments = bundleOf(
+            "query" to query,
+            "result" to result.toJson()
+        )
+    }
+}
+
+class HouseMoreResultFragment : HouseSearchResultFragment(), MoreResultFragment<House> {
+
+    override val limit: Int = Int.MAX_VALUE
+
+    override val title: String
+        get() = """บ้าน "${arguments!!.getString("query")}" """.trim()
+
+    val result: List<House>
+        get() = arguments!!.getAs("result")!!
+
+    override fun setArgument(query: String, result: List<House>) {
+        arguments = bundleOf(
+            "query" to query,
+            "result" to result.toJson()
+        )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.content.value = result
+        header.requestScroll()
+    }
+}
