b3db4866356c7508c4bc52d109f41397239e4325
Piruin Panichphol <piruin.p@gmail.com>
2018-12-18 14:10:55 +0700

    Show patient information on HomeVisit Activity

--- ffc/src/main/kotlin/ffc/android/Glide.kt
+++ ffc/src/main/kotlin/ffc/android/Glide.kt
@@ -1,10 +1,15 @@
 package ffc.android
 
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import org.jetbrains.anko.dip
 
 fun ImageView.load(uri: Uri, requestOptions: RequestOptions.() -> Unit = {}) {
     Glide.with(this)
@@ -15,4 +20,20 @@ fun ImageView.load(uri: Uri, requestOptions: RequestOptions.() -> Unit = {}) {
             .skipMemoryCache(false)
             .apply(requestOptions))
         .into(this)
+}
+
+fun TextView.loadDrawableBottom(uri: Uri, requestOptions: RequestOptions.() -> Unit = {}) {
+    Glide.with(this)
+        .load(uri)
+        .apply(RequestOptions.circleCropTransform())
+        .apply(RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .skipMemoryCache(false)
+            .apply(requestOptions))
+        .into(object : SimpleTarget<Drawable>(dip(24), dip(24)) {
+
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                this@loadDrawableBottom.drawableBottom = resource
+            }
+        })
 }
