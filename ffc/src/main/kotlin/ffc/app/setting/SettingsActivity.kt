f8131076c90a1700a80d77d973005282a9d6ddbf
Piruin Panichphol <piruin.p@gmail.com>
2019-02-21 15:40:44 +0700

    Can change api server on debug version

--- ffc/src/main/kotlin/ffc/app/setting/SettingsActivity.kt
+++ ffc/src/main/kotlin/ffc/app/setting/SettingsActivity.kt
@@ -1,11 +1,17 @@
 package ffc.app.setting
 
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.support.v7.preference.EditTextPreference
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
+import ffc.api.FfcCentral
+import ffc.app.BuildConfig
 import ffc.app.FamilyFolderActivity
 import ffc.app.R
+import org.jetbrains.anko.support.v4.toast
 
 class SettingsActivity : FamilyFolderActivity(),
     PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
@@ -40,10 +46,44 @@ class SettingsActivity : FamilyFolderActivity(),
         }
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
         override fun onCreatePreferences(bundle: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.pref_settings, rootKey)
+            setPreferencesFromResource(ffc.app.R.xml.pref_settings, rootKey)
+        }
+
+        override fun onActivityCreated(savedInstanceState: Bundle?) {
+            super.onActivityCreated(savedInstanceState)
+
+            preferenceScreen.sharedPreferences.edit().putString("api_url", FfcCentral.url).apply()
+
+            with(findPreference("api_url")) {
+                this as EditTextPreference
+                summary = FfcCentral.url
+                text = FfcCentral.url
+                isVisible = BuildConfig.DEBUG
+            }
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+            when (key) {
+                "api_url" -> {
+                    toast("updated API address!")
+                    val newValue = sharedPreferences!!.getString(key, null)
+                    findPreference("api_url").summary = newValue
+                    FfcCentral.saveUrl(context!!, Uri.parse(newValue!!))
+                }
+            }
         }
     }
 }
