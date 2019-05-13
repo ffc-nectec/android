46e44dced35bc46de553c9b5637b87ebbe5a8304
Piruin Panichphol <piruin.p@gmail.com>
2019-05-13 14:18:57 +0700

    fix

--- ffc/src/main/kotlin/ffc/app/health/analyze/HealthIssues.kt
+++ ffc/src/main/kotlin/ffc/app/health/analyze/HealthIssues.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2019 NECTEC
+ *   National Electronics and Computer Technology Center, Thailand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ffc.app.health.analyze
 
 import ffc.api.ApiErrorException
@@ -5,6 +22,7 @@ import ffc.api.FfcCentral
 import ffc.app.mockRepository
 import ffc.app.util.RepoCallback
 import ffc.entity.Person
+import ffc.entity.healthcare.Severity
 import ffc.entity.healthcare.analyze.HealthAnalyzer
 import ffc.entity.healthcare.analyze.HealthChecked
 import ffc.entity.healthcare.analyze.HealthIssue
@@ -26,10 +44,10 @@ internal class DummyHealthIssue : HealthIssues {
     override fun issueOf(person: Person, dsl: RepoCallback<Collection<HealthIssue>>.() -> Unit) {
         val callback = RepoCallback<Collection<HealthIssue>>().apply(dsl)
         val issues = listOf(
-            HealthProblem(HealthIssue.Issue.HT, HealthIssue.Severity.HI),
-            HealthProblem(HealthIssue.Issue.DM, HealthIssue.Severity.LOW),
-            HealthProblem(HealthIssue.Issue.CVD, HealthIssue.Severity.VERY_HI),
-            HealthProblem(HealthIssue.Issue.ACTIVITIES, HealthIssue.Severity.MID),
+            HealthProblem(HealthIssue.Issue.HT, Severity.HI),
+            HealthProblem(HealthIssue.Issue.DM, Severity.LOW),
+            HealthProblem(HealthIssue.Issue.CVD, Severity.VERY_HI),
+            HealthProblem(HealthIssue.Issue.ACTIVITIES, Severity.MID),
             HealthChecked(HealthIssue.Issue.FALL_RISK),
             HealthChecked(HealthIssue.Issue.CATARACT, haveIssue = true)
         )
